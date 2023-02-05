70fd289ebe4f1f0499bc23870d85b994c3d5b716
Andrew Leppard <andrew@leppard.id.au>
2023-02-05 20:27:29 +1030

    Added an empty query result for when there are no matches. This is used for a local-only search.

---
+++ src/main/java/com/argus/EmptyQueryResult.java
@@ -0,0 +1,50 @@
+package com.argus;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import freemarker.template.Template;
+
+/**
+ * Representation of an empty query result, i.e. a query that had
+ * no results.
+ */
+public class EmptyQueryResult implements QueryResult {
+
+    private static final Logger LOGGER =
+        Logger.getLogger(EmptyQueryResult.class.getName());
+
+    private String query;
+
+    public EmptyQueryResult(final String query) {
+        this.query = query;
+    }
+    
+    @Override public void setResponse(HttpServletResponse response)
+        throws IOException {
+
+         TemplateEngine templateEngine = new TemplateEngine();
+
+         Map<String, Object> arguments = new HashMap<>();
+         arguments.put("query", query);
+
+         try {
+             Template template =
+                 templateEngine.getTemplate("templates/empty-query-result.html.ftl");
+             template.process(arguments, response.getWriter());
+             response.setStatus(HttpServletResponse.SC_OK);
+         }
+         catch (Exception exception) {
+             LOGGER.log(Level.SEVERE, "Error returning empty query result.",
+                        exception);            
+             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+         }
+     }
+}
---
+++ src/main/resources/templates/empty-query-result.html.ftl
@@ -0,0 +1,25 @@
+<#import "./core.ftl" as core>
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <@core.meta/>
+    <style>
+      <@core.style/>
+      .result {
+        display: flex;
+        justify-content: center; 
+        color: #eeeeee;
+        font-family: sans-serif;
+        font-size: 18px;
+        padding-top: 64px;
+      }    
+    </style>
+  </head>
+  <body>
+    <@core.script/>
+    <@core.query query/>
+    <div class="result">
+      Not results found.
+    </div>
+  </body>
+</html>
