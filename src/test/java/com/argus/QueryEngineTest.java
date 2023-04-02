6c01d38b5b0ac1369d39ea2b2742f511d65789a7
Andrew Leppard <andrew@leppard.id.au>
2023-04-02 11:02:39 +0930

    Initial version.

---
+++ src/test/java/com/argus/QueryEngineTest.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////
+package com.argus;
+
+import org.junit.Test;
+
+/**
+ * Tests QueryEngine class.
+ */
+public class QueryEngineTest {
+
+    /** 
+     * Test that we can initialise the QueryEngine. We should
+     * do an end-to-end test to actually test this class.
+     */
+    @Test public void test() {
+        QueryEngine engine = new QueryEngine();
+    }
+}
