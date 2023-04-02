103aecba7bb38c5c0ee379b42d9ccb79ae0bedfc
Andrew Leppard <andrew@leppard.id.au>
2023-04-02 11:18:32 +0930

    Added support for resolving unicode code points.

--- src/main/java/com/argus/resolver/UnicodeResolver.java
+++ src/main/java/com/argus/resolver/UnicodeResolver.java
@@ -11,7 +11,10 @@ import java.util.List;
 import java.util.logging.Logger;
 
 /**
- * Returns lists of unicode characters matching the given name.
+ * Returns:
+ *
+ * i. lists of unicode characters matching the given name OR
+ * ii. The unicode character with the given code point (e.g. U+12).
  *
  * @todo When searching for characters such as "a" it would be
  * better only to match "A" and not any word with the letter A.
@@ -49,16 +52,66 @@ public class UnicodeResolver implements Resolver
             }
         }
     }
-    
-    public @Override QueryResult tryResolve(final Query query) {
 
+    private static void addUnicodeCharacter(TableQueryResult result,
+                                            UnicodeCharacter character) {
+        result.addRow
+            (new TableQueryResult.Cell("&#" + character.codePoint + ";",
+                                       false, false),
+             new TableQueryResult.Cell(character.name),
+             new TableQueryResult.Cell
+             ("U+" + Integer.toHexString(character.codePoint)
+              .toUpperCase()));
+    }
+
+    private QueryResult tryResolveByCodePoint(final Query query) {
+        List<String> words = 
+            new ArrayList<>(Arrays.asList(query.getRawString()
+                                          .toUpperCase().split(" ")));
+        if (words.size() < 1) return null;
+        
+        // Remove "unicode" if it appears.
+        if (words.get(0).equals("UNICODE")) {
+            words.remove(0);
+        }
+        if (words.get(words.size() - 1).equals("UNICODE")) {
+            words.remove(words.size() - 1);
+        }
+
+        if (words.size() != 1) return null;
+        final String string = words.get(0).trim();
+        if (!string.startsWith("U+")) {
+            return null;
+        }
+
+        try {
+            final int codePoint = Integer.parseInt(string.substring(2), 16);
+            
+            TableQueryResult result = new TableQueryResult(query);
+
+            final UnicodeCharacter character = unicodeCharacters
+                .stream()
+                .filter(item -> item.codePoint == codePoint)
+                .findFirst()
+                .orElse(null);
+            if (character == null) return null;
+
+            addUnicodeCharacter(result, character);
+            return result;
+        }
+        catch (NumberFormatException exception) {
+            return null;
+        }
+    }
+
+    private QueryResult tryResolveByNameSearch(final Query query) {
         // @todo Move this to Query.
         // @todo Run trim() over all words.
         List<String> words = 
             new ArrayList<>(Arrays.asList(query.getRawString()
                                           .toUpperCase().split(" ")));
         if (words.size() < 2) return null;
-
+        
         // Only match queries with the first or last word being unicode.
         if (words.get(0).equals("UNICODE")) {
             words.remove(0);
@@ -81,18 +134,21 @@ public class UnicodeResolver implements Resolver
                     break;
                 }
             }
-
-            if (!match) continue;
             
-            result.addRow
-                (new TableQueryResult.Cell("&#" + character.codePoint + ";",
-                                           false, false),
-                 new TableQueryResult.Cell(character.name),
-                 new TableQueryResult.Cell
-                 ("U+" + Integer.toHexString(character.codePoint)
-                  .toUpperCase()));
+            if (!match) continue;
+
+            addUnicodeCharacter(result, character);
         }
         
         return result.getRowCount() > 0? result : null;
+    }
+    
+    public @Override QueryResult tryResolve(final Query query) {
+        QueryResult result = tryResolveByNameSearch(query);
+        if (result == null) {
+            result = tryResolveByCodePoint(query);
+        }
+
+        return result;
     }
 }
